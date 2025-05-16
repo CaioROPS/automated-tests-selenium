1f87b794281d3b73013b5a737eee8230c10c7902
Caio Ricardo <103211365+CaioROPS@users.noreply.github.com>
2025-05-16 08:57:22 -0300

    Update create_user.py

--- Tests/create_user.py
+++ Tests/create_user.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 import time
 
 def test_create_employee(login, user_data):
